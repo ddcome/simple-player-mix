21f99c5b0b045bf9b9976c017c962bb56387492d
Maynard Lee <lxd_ddcome@163.com>
2025-03-18 14:24:48 +0800

    Create index.js

---
+++ screens/index.js
@@ -0,0 +1,28 @@
+// 获取./路径下的所有文件
+import {infoLog} from "./../util/logger";
+
+const requireComponent = require.context(
+    './', // 组件所在目录的相对路径
+    true, // 是否查询其子目录
+    /\w+\.template\.vue$/ // 匹配***Layer.vue的组件
+)
+let comObj = {}
+requireComponent.keys().forEach(fileName => {
+    // 获取文件名
+    let names = fileName.split('/').pop().replace('.template.vue', '')
+    // 获取组件配置
+    const componentConfig = requireComponent(fileName)
+    // 若该组件是通过"export default"导出的，优先使用".default"，否则退回到使用模块的根
+    comObj[names] = componentConfig.default || componentConfig
+});
+
+/**
+* @function
+* @description 导出所有的组件Layer
+* @return {String}
+* @author lixiaodong31 2023/9/8
+* @version 1.0.0
+* @example
+* components: Object.assign({}, allCompLayer, { a: a })
+*/
+export const allCompTemplate = comObj;
