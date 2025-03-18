4b2a3fa965f5e8f6d483bfb6c46d0d7d6c35dafd
Maynard Lee <lxd_ddcome@163.com>
2025-03-18 14:19:11 +0800

    Create vertical_3_2_multiply.template.vue

---
+++ screens/vertical_3_2_multiply.template.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="vertical-3-2-multiply common-template-root" >
+        <div
+            class="v-item common-screen-item"
+            v-for="index in 6"
+            :key="getId(index)"
+            :data-index="getId(index)"
+            v-selected="getSelectedId(getId(index))"
+            @mouseenter="mouseenterHandle"
+            @mouseleave="mouseleaveHandle"
+            @mouseup.left="mouseupHandle(getId(index))"
+        >
+            <slot :index="getId(index)"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import {MainMixin} from "./main.mixin";
+
+export default {
+    name: "vertical_3_2_multiply",
+    mixins: [MainMixin],
+}
+</script>
+
+<style lang="scss" scoped>
+@import "common";
+.vertical-3-2-multiply {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    .v-item {
+        position: relative;
+        width: 33.33%;
+        height: 50%;
+    }
+}
+</style>
